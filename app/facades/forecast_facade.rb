eecab1986eecba1a6341a7263cf4ed6a18f46e5c
JoannaCoPo <jococopo@gmail.com>
2021-09-25 12:48:12 -0600

    Fix naming convention

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,8 +1,8 @@
 class ForecastFacade
   class << self
-    def local_weather
-      response = OpenWeatherService.get_forecast(lat,long)
-      Forecast.new(response)
+    def local_weather(lat, long)
+      data = OpenWeatherService.get_forecast(lat,long)
+      Forecast.new(data)
     end
   end
 end
