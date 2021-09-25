ede97532953ffa5ebc4b1abdce2d56fb14115088
JoannaCoPo <jococopo@gmail.com>
2021-09-25 14:07:16 -0600

    Fix naming

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     location = GeocodingFacade.get_lat_long(params[:location])
-    local_weather = ForecastFacade.local_weather(location.lat, location.long)
-    render json: ForecastSerializer.new(current_weather)
+    current = ForecastFacade.local_weather(location.lat, location.long)
+    render json: ForecastSerializer.new(current)
   end
 end
 
