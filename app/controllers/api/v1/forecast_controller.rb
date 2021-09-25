1412a9320bc3a3a73a543eb3f44fc5470ad721c5
JoannaCoPo <jococopo@gmail.com>
2021-09-25 12:48:46 -0600

    Add forecast serializer

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    # need to grab lat/long params from mapquest
-    location = params[:location]
+    location = GeocodingFacade.get_lat_long(params[:location])
     local_weather = ForecastFacade.local_weather(location.lat, location.long)
+    render json: ForecastSerializer.new(current_weather)
   end
 end
+
+# maybe add conditional: error for when no location is provided
