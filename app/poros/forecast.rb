8865e6ba8b6376094301b067fc2796544e48cc52
JoannaCoPo <jococopo@gmail.com>
2021-09-25 14:08:11 -0600

    Create daily weather method

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,23 +1,23 @@
 class Forecast
-  attr_reader :current_weather
+  attr_reader :current_weather, :id
   def initialize(data) #pass this to current_weather obj?
     # create a current_weather poros to store all of those attributes and the call here?
-    @current_weather = CurrentWeather.new()
+    require "pry"; binding.pry
+    @current_weather = CurrentWeather.new(data[:current])
+    @id = nil # id is a mandatory field in the jsonapi spec
+    @daily_weather = data[:daily]
+  end
+
+  def daily_weather
+    next_five_days = @daily_weather.first(5)
+    next_five_days.map do |day|
+      DailyWeather.new(day)
+    end
   end
 end
 
 # will need:
-  # current_weather:
-    # datetime, in a human-readable format such as “2020-09-30 13:27:03 -0600”
-    # sunrise, in a human-readable format such as “2020-09-30 06:27:03 -0600”
-    # sunset, in a human-readable format such as “2020-09-30 18:27:03 -0600”
-    # temperature, floating point number indicating the current temperature in Fahrenheit
-    # feels_like, floating point number indicating a temperature in Fahrenheit
-    # humidity, numeric (int or float), as given by OpenWeather
-    # uvi, numeric (int or float), as given by OpenWeather
-    # visibility, numeric (int or float), as given by OpenWeather
-    # conditions, the first ‘description’ field from the weather data as given by OpenWeather
-    # icon, string, as given by OpenWeather
+  # current_weather
   # daily_weather, array of the next 5 days of daily weather data
     # date, in a human-readable format such as “2020-09-30”
     # sunrise, in a human-readable format such as “2020-09-30 06:27:03 -0600”
