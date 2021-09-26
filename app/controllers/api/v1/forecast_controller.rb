57ac098b86066904ddfe128f997278bb1ac708ef
JoannaCoPo <jococopo@gmail.com>
2021-09-25 21:56:32 -0600

    Utilize ternary operator for error handling

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,9 +1,16 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    location = GeocodingFacade.get_lat_long(params[:location])
-    current = ForecastFacade.local_weather(location.lat, location.long)
-    render json: ForecastSerializer.new(current)
+    params[:location] ? initialize_serializer : invalid_location
   end
 end
 
-# maybe add conditional: error for when no location is provided
+private
+def initialize_serializer
+  location = GeocodingFacade.get_lat_long(params[:location])
+  current = ForecastFacade.local_weather(location.lat, location.long)
+  render json: ForecastSerializer.new(current)
+end
+
+def invalid_location
+  render json: { errors: 'No location specified' }, status: 400
+end
