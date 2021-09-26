71e050a15c081b58f495b098d9144d3fd75c158e
JoannaCoPo <jococopo@gmail.com>
2021-09-25 21:57:06 -0600

    Test GET forecast endpoint

--- spec/requests/api/v1/forecast/index_spec.rb
+++ spec/requests/api/v1/forecast/index_spec.rb
@@ -7,11 +7,27 @@ describe 'forecast request' do
       get '/api/v1/forecast?location=denver,co'
 
       forecast = JSON.parse(response.body, symbolize_names: true)
+      attributes = forecast[:data][:attributes]
 
-      expect(forecast).to be_successful
+      expect(response).to be_successful
       expect(forecast).to be_a(Hash)
+      expect(attributes).to have_key(:current_weather)
+      expect(attributes).to have_key(:daily_weather)
+      expect(attributes).to have_key(:hourly_weather)
+    end
+  end
+
+  describe 'sad path' do
+    it 'returns an eror for invalid location', :vcr do
+
+      get '/api/v1/forecast?'
+      # if time, add coverage for 'location='
+
+      forecast = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      expect(forecast[:errors]).to eq('No location specified')
     end
   end
 end
-
-# add sad path
