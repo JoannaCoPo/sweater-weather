07f1d5de512fe8484baacbf6309423c326d239af
JoannaCoPo <jococopo@gmail.com>
2021-09-26 11:59:04 -0600

    Test error coverage

--- spec/facades/geocoding_facade_spec.rb
+++ spec/facades/geocoding_facade_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe 'geocoding facade' do
     location = GeocodingFacade.get_lat_long('denver,co')
     expect(location).to be_a(Location)
   end
+
+  it 'returns error if no location specified', :vcr do
+    actual = GeocodingFacade.get_lat_long('')
+    expected = 'No location specified'
+    expect(actual).to eq(expected)
+  end
 end
