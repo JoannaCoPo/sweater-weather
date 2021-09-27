1c3cec59e61fd758300f30638fc9625abb1233bf
JoannaCoPo <jococopo@gmail.com>
2021-09-27 15:49:45 -0600

    Add has_secure_token method for api_key

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true, confirmation: true
   has_secure_password
+  has_secure_token :api_key # default 24 characters
 end
