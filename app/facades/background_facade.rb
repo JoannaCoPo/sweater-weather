849d60eefe9fedac73bb16fd04ce1c5c5815f593
JoannaCoPo <jococopo@gmail.com>
2021-09-26 20:40:12 -0600

    Create background facade with get location image method

---
+++ app/facades/background_facade.rb
@@ -0,0 +1,12 @@
+class BackgroundFacade
+  def self.get_location_image(location)
+    response = UnsplashService.get_images(location)
+    if location.nil? || location.empty?
+      return 'Invalid location'
+    else
+      results_data = response[:results][0]
+      results_data[:location] = location
+      Image.new(results_data)
+    end
+  end
+end
