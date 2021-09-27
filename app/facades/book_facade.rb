c2e7d90c1d1a6cbf458e6147e941bafa381ff8dc
JoannaCoPo <jococopo@gmail.com>
2021-09-27 10:22:21 -0600

    Pass required data to Book poro from facade

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -3,12 +3,12 @@ class BookFacade
     def books_by_location(location, quantity)
       forecast = weather(location)
       response = LibraryService.get_books(location, quantity)
-      Book.new(location, forecast)
+      Book.new(location, forecast, response[:numFound], response[:docs])
     end
 
     def weather(location)
-      GeocodingFacade.get_lat_long(location)
-      ForecastFacade.local_weather(location.lat, location.long).current_weather
+      coords = GeocodingFacade.get_lat_long(location)
+      ForecastFacade.local_weather(coords.lat, coords.long).current_weather
     end
   end
 end
--- app/poros/book.rb
+++ app/poros/book.rb
@@ -1,7 +1,16 @@
 class Book
-  attr_reader :id
-  def initialize(books_data)
+  attr_reader :id,
+              :destination_city,
+              :forecast,
+              :books_found
+  def initialize(location, forecast, quantity, info)
     @id = nil
     require "pry"; binding.pry
+    @destination_city = location
+    @forecast = forecast
+    @books_found = quantity
+    @info = info
   end
 end
+
+# need to get bookinfo and forecast for EACH book
