9ef7a0fbe78086ace67222ae53ed467d6553ddab
JoannaCoPo <jococopo@gmail.com>
2021-09-27 10:56:14 -0600

    Create books and forecast methods

--- app/poros/book.rb
+++ app/poros/book.rb
@@ -1,15 +1,30 @@
 class Book
   attr_reader :id,
               :destination_city,
-              :forecast,
-              :books_found
+              :total_books_found
   def initialize(location, forecast, quantity, info)
     @id = nil
-    require "pry"; binding.pry
     @destination_city = location
     @forecast = forecast
-    @books_found = quantity
+    @total_books_found = quantity
     @info = info
+  end
+
+  def books
+    @info.map do |book|
+      {
+        isbn: book[:isbn],
+        title: book[:title],
+        publisher: book[:publisher]
+      }
+    end
+  end
+
+  def forecast
+    {
+      summary: @forecast.conditions,
+      temperature: "#{@forecast.temperature} F"
+    }
   end
 end
 
