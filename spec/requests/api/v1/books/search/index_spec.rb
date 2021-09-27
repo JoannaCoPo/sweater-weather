0a24b06e804b2a0f6a8dba140d151a96e2176667
JoannaCoPo <jococopo@gmail.com>
2021-09-27 10:56:41 -0600

    Test book search

--- spec/requests/api/v1/books/search/index_spec.rb
+++ spec/requests/api/v1/books/search/index_spec.rb
@@ -4,11 +4,22 @@ describe 'books request' do
   describe 'happy path' do
     it 'can retrieve books for a specified locaiton', :vcr do
 
-      get '/api/v1/books-searchlocation=denver,co&quantity=5'
-      books = JSON.parse(response.body, symbolize_names: true)
+      get '/api/v1/book-search?location=denver,co&quantity=5'
+      results = JSON.parse(response.body, symbolize_names: true)
+      books = results[:data]
+      attributes =  books[:attributes]
 
       expect(response).to be_successful
-      expect(books).to be_a(Hash)
+      expect(results).to be_a(Hash)
+      expect(books[:type]).to eq('books')
+      expect(attributes[:destination_city]).to eq('denver,co')
+      expect(attributes[:forecast][:summary]).to eq('scattered clouds')
+      expect(attributes[:forecast][:temperature]).to eq('75.45 F')
+      expect(attributes[:total_books_found]).to eq(35990)
+      expect(attributes[:books].count).to eq(5)
+      expect(attributes[:books][1]).to have_key(:isbn)
+      expect(attributes[:books][2]).to have_key(:title)
+      expect(attributes[:books][3]).to have_key(:publisher)
     end
   end
 
