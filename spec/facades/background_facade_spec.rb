3193dcb2fb52803d055c1ae821f8a5cfc337a3fa
JoannaCoPo <jococopo@gmail.com>
2021-09-26 22:30:18 -0600

    Test error coverage

--- spec/facades/background_facade_spec.rb
+++ spec/facades/background_facade_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe 'background facade' do
     expect(image.location).to eq('denver,co')
     expect(image.credit[:author]).to eq('Ryan De Hamer')
   end
+
+  it 'returns an error if no location is specified', :vcr do
+    actual = BackgroundFacade.get_location_image('')
+    expected = 'Invalid location'
+    expect(actual).to eq(expected)
+  end
 end
