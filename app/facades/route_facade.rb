6a195d1ca0d38ee07f68faa536eb699439a40772
JoannaCoPo <jococopo@gmail.com>
2021-09-28 15:57:09 -0600

    Add coverage for overseas attempt

--- app/facades/route_facade.rb
+++ app/facades/route_facade.rb
@@ -2,10 +2,13 @@ class RouteFacade
   class << self
     def get_route_info(start_city, end_city)
       results = MapquestService.get_route(start_city, end_city)
-      # maybe error handling conditional here?
-      location = GeocodingFacade.get_lat_long(end_city)
-      weather = OpenWeatherService.get_forecast(location.lat,location.long)[:hourly]
-      RoadTripInfo.new(results[:route], weather)
+      if results[:info][:statuscode] == 0
+        location = GeocodingFacade.get_lat_long(end_city)
+        weather = OpenWeatherService.get_forecast(location.lat,location.long)[:hourly]
+        RoadtripInfo.new(results[:route], weather)
+      else
+        InvalidRoute.new(start_city, end_city)
+      end
     end
   end
 end
