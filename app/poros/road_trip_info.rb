2bc23dcce45f6e6cc88f49412c3a27846cda5e36
JoannaCoPo <jococopo@gmail.com>
2021-09-28 12:55:37 -0600

    Start to manipulate route data for attributes

--- app/poros/road_trip_info.rb
+++ app/poros/road_trip_info.rb
@@ -1,15 +1,28 @@
 class RoadTripInfo
   attr_reader :id,
-              :start_city,
-              :end_city,
+              # :start_city,
+              # :end_city,
               :travel_time,
-              :weather_at_etag
+              :weather_at_eta
   def initialize(route_data, weather)
     require "pry"; binding.pry
     @id = nil
+    @location_data = route_data[:locations]
     # @start_city
     # @end_city
     # @travel_time
     # @weather_at_eta
+  end
+
+  def start_city
+    city = @location_data[0][:adminArea5]
+    state = @location_data[0][:adminArea3]
+    city.concat(", ", state)
+  end
+
+  def end_city
+    city = @location_data[1][:adminArea5]
+    state = @location_data[1][:adminArea3]
+    city.concat(", ", state)
   end
 end
