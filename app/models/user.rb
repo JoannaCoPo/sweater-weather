9e61e2e6287ca97c3af564da07585aa2a6779e09
JoannaCoPo <jococopo@gmail.com>
2021-09-27 19:46:39 -0600

    Format email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  validates :email, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, confirmation: true
   has_secure_password
   has_secure_token :api_key # default 24 characters
 end
+
+# validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+# constant built into URI in the standard ruby library
