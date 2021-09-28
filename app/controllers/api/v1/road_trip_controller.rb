70e84f748aaefeaf3b1c561f20eb900d31bba44f
JoannaCoPo <jococopo@gmail.com>
2021-09-28 15:57:21 -0600

    Remove comments

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -9,15 +9,3 @@ class Api::V1::RoadTripController < ApplicationController
     end
   end
 end
-
-# a data attribute, under which all other attributes are present:
-  # id, always set to null
-  # type, always set to “roadtrip”
-  # attributes, an object containing road trip information:
-    # start_city, string, such as “Denver, CO”
-    # end_city, string, such as “Estes Park, CO”
-    # travel_time, string, something user-friendly like “2 hours, 13 minutes” or “2h13m” or “02:13:00” or something of that nature (you don’t have to include seconds); set this string to “impossible route” if there is no route between your cities
-    # weather_at_eta, conditions at end_city when you arrive (not CURRENT weather), object containing:
-      # temperature, numeric value in Fahrenheit
-      # conditions, string, as given by OpenWeather
-      # note: this object will be blank if the travel time is impossible
