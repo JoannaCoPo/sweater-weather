4e1cd00e6eafc70460a6bb5f9ff999fac39e2c02
JoannaCoPo <jococopo@gmail.com>
2021-09-28 14:13:25 -0600

    Create weather_at_eta attribute

--- app/poros/road_trip_info.rb
+++ app/poros/road_trip_info.rb
@@ -1,17 +1,11 @@
 class RoadTripInfo
-  attr_reader :id,
-              # :start_city,
-              # :end_city,
-              :travel_time,
-              :weather_at_eta
+  attr_reader :id, :weather_at_eta, :time_to_arrival
   def initialize(route_data, weather)
-    require "pry"; binding.pry
     @id = nil
     @location_data = route_data[:locations]
-    # @start_city
-    # @end_city
-    # @travel_time
-    # @weather_at_eta
+    @format_time = route_data[:formattedTime].split(':')
+    @time_to_arrival = route_data[:formattedTime].split(':')[0].to_i
+    @weather_at_eta = weather[@time_to_arrival]
   end
 
   def start_city
@@ -24,5 +18,19 @@ class RoadTripInfo
     city = @location_data[1][:adminArea5]
     state = @location_data[1][:adminArea3]
     city.concat(", ", state)
+  end
+
+  def travel_time
+    hours = @format_time[0].to_i
+    minutes = @format_time[1].to_i
+    "#{hours} hour(s), #{minutes} minute(s)"
+  end
+
+  def weather_at_eta #what if forecast is not available?
+    weather  = HourlyWeather.new(@weather_at_eta)
+    {
+      temperature: weather.temperature,
+      conditions: weather.conditions
+    }
   end
 end
