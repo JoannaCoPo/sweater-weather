00bf40408a6347147fd23f3cdc9a6174d018a4c9
JoannaCoPo <jococopo@gmail.com>
2021-09-27 19:46:16 -0600

    Test user creation request

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      # generate new api key? or just call here
       user.api_key
       render json: UserSerializer.new(user), status: 201
     else
@@ -12,7 +11,7 @@ class Api::V1::UsersController < ApplicationController
 # why is :id nested before :type in reponse?
   private
   def user_params
-    params.permit(:email, :password, :password_confirmation, :api_key) # probably api_key as params too
+    params.permit(:email, :password, :password_confirmation, :api_key) 
   end
 end
 
--- spec/requests/api/v1/users/create_spec.rb
+++ spec/requests/api/v1/users/create_spec.rb
@@ -12,19 +12,79 @@ describe 'user creation request' do
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
               }
-# need to send json payload
+
       post '/api/v1/users', headers: headers, params: body.to_json
 
       user = JSON.parse(response.body, symbolize_names: true)
+      attributes = user[:data][:attributes]
 
       expect(response).to be_successful
       expect(response.status).to eq(201)
       expect(user).to be_a(Hash)
+      expect(user[:data][:type]).to eq('user')
+      expect(attributes).to have_key(:email)
+      expect(attributes).to have_key(:api_key)
+      expect(attributes).to_not have_key(:password)
     end
   end
 
-  # describe 'sad path' do
-  #   it 'returns an eror for invalid location', :vcr do
-  #   end
-  # end
+  describe 'sad path' do
+    it 'returns an error if passwords don not match', :vcr do
+      body = {
+               'email':                 'email@email.com',
+               'password':              'password',
+               'password_confirmation': 'bad_password'
+             }
+      headers = {
+                 'Content-Type': 'application/json',
+                 'Accept': 'application/json'
+               }
+
+       post '/api/v1/users', headers: headers, params: body.to_json
+       user = JSON.parse(response.body, symbolize_names: true)
+
+       expect(response).to_not be_successful
+       expect(response.status).to eq(400)
+       expect(user[:password_confirmation][0]).to eq("doesn't match Password")
+    end
+
+    it 'returns an error if form is incomplete', :vcr do
+      body = {
+               'email':                 '',
+               'password':              'password',
+               'password_confirmation': 'password'
+             }
+      headers = {
+                 'Content-Type': 'application/json',
+                 'Accept': 'application/json'
+               }
+
+       post '/api/v1/users', headers: headers, params: body.to_json
+       user = JSON.parse(response.body, symbolize_names: true)
+
+       expect(response).to_not be_successful
+       expect(response.status).to eq(400)
+       expect(user[:email][0]).to eq("can't be blank")
+    end
+
+    it 'returns an error if email already exists', :vcr do
+      User.create!(email: 'test@test.com', password: 'password', password_confirmation: 'password', api_key: 'hocuspocus')
+      body = {
+               'email':                 'test@test.com',
+               'password':              'password',
+               'password_confirmation': 'password'
+             }
+      headers = {
+                 'Content-Type': 'application/json',
+                 'Accept': 'application/json'
+               }
+
+       post '/api/v1/users', headers: headers, params: body.to_json
+       user = JSON.parse(response.body, symbolize_names: true)
+
+       expect(response).to_not be_successful
+       expect(response.status).to eq(400)
+       expect(user[:email][0]).to eq("has already been taken")
+    end
+  end
 end
