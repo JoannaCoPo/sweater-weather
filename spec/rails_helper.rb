317a5798ad1b5bfda3420973f582b31fa2df7db5
JoannaCoPo <jococopo@gmail.com>
2021-10-09 00:15:38 -0600

    Update config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,6 +64,7 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |config|
+  config.allow_http_connections_when_no_cassette = false
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
   config.filter_sensitive_data('<appid>') { ENV['OPEN_WEATHER_KEY'] }
